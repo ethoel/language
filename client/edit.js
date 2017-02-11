2511ca21aa97e4658c01a56d86af217e82cd26ed
Elliott Hoel <elliott.hoel@gmail.com>
2017-02-11 17:41:44 -0500

    Added tags interface

--- client/edit.js
+++ client/edit.js
@@ -1,3 +1,15 @@
+// local copy of study's image array and tag arrays
+var editImageArray = [];
+var editStudyTags = [];
+
+var setCurrentStudy = function (studyName) {
+  var study = Studies.findOne({ name: studyName });
+  editImageArray = study.imageArray;
+  Session.set("currentStudy", studyName);
+  Session.set("updateReactive", editImageArray[0]);
+  setFieldsDisabled(true);
+}
+
 var displayCancelSaveButton = function () {
   $("#editStudy").css({"display": "none"});
   $("#cancelStudy").css({"display": "inline"});
@@ -10,16 +22,19 @@ var displayEditButton = function () {
   $("#saveStudy").css({"display": "none"});
 };
 
-var setFieldsReadOnly = function (readOnly) {
-  $("#currentStudyAddress").attr("readonly", readOnly);
-  $("#currentStudyTitle").attr("readonly", readOnly);
+var setFieldsDisabled = function (disabled) {
+  $("#currentStudyAddress").attr("disabled", disabled);
+  $("#currentStudyTitle").attr("disabled", disabled);
+  $("#allTagsDropDown").attr("disabled", disabled);
+  $("#addStudyTagButton").attr("disabled", disabled);
+  $(".studyTagButton").attr("disabled", disabled);
 };
 
 var resetAllFields = function () {
   var study = Studies.findOne({ name: Session.get("currentStudy") });
   $("#currentStudyAddress").val(study.name);
   $("#currentStudyTitle").val(study.title);
-}
+};
 
 var saveAllFields = function () {
   // get all the entered values
@@ -57,10 +72,22 @@ var saveAllFields = function () {
   $("#currentOrganDrop").val(newStudyName).change();
   Session.set("currentStudy", newStudyName);
   return true;
-}
+};
+
+var swapImageWithNextImage = function (indexA) {
+  var indexB = indexA + 1;
+  var tmpImageID = editImageArray[indexA];
+  if (indexB < editImageArray.length) {
+    editImageArray[indexA] = editImageArray[indexB];
+    editImageArray[indexB] = tmpImageID;
+    // note that A was swapped with B
+    tmpImageID = tmpImageID + editImageArray[indexA];
+  }
+  return tmpImageID;
+};
 
 Template.edit.onRendered(function () {
-  Session.set("currentStudy", $("#studiesDropDown option:selected").val());
+  setCurrentStudy($("#studiesDropDown option:selected").val());
 });
 
 Template.edit.helpers({
@@ -78,22 +105,58 @@ Template.edit.helpers({
     }
     return studies;
   },
+  allTags: function () {
+    var addedStudyTag = Session.get("updateReactive");
+    var tags = ["Normal", "Abnormal", "Ultrasound"];
+    tags.push("Custom...");
+    return tags;
+  },
+  studyTags: function () {
+    var addedStudyTag = Session.get("updateReactive");
+    var studyTags = editStudyTags;
+    return studyTags;
+  },
+  verified: function () {
+    // TODO
+    return "checked";
+  },
+  studyOwner: function () {
+    var study = Studies.findOne({ name: Session.get("currentStudy") });
+    var studyOwner;
+    if (study) {
+      studyOwner = study.owner;
+    } else {
+      studyOwner = "";
+    }
+    return studyOwner;
+  },
   studyTitle: function () {
-    var studyName = Session.get("currentStudy");
-    var study = Studies.findOne({ name: studyName });
-    return study.title;
+    var study = Studies.findOne({ name: Session.get("currentStudy") });
+    var studyTitle;
+    if (study) {
+      studyTitle = study.title;
+    } else {
+      studyTitle = "";
+    }
+    return studyTitle;
   },
   studyAddress: function () {
-    var studyName = Session.get("currentStudy");
-    var study = Studies.findOne({ name: studyName });
-    return study.name;
+    var study = Studies.findOne({ name: Session.get("currentStudy") });
+    var studyAddress;
+    if (study) {
+      studyAddress = study.name;
+    } else {
+      studyAddress = "";
+    }
+    return studyAddress;
   },
   images: function () {
-    var study = Studies.findOne({ name: Session.get("currentStudy") });
+    // not using imageZero, but makes this responsive
+    var swappedImageID = Session.get("updateReactive");
     var imageURLs = [];
-    for (var i = 0; i < study.imageArray.length; i++) {
-      var imageFile = Images.findOne({_id: study.imageArray[i]});
-      imageURLs[i] = imageFile.url();
+    for (var i = 0; i < editImageArray.length; i++) {
+      var imageFile = Images.findOne({_id: editImageArray[i]});
+      imageURLs[i] = { url: imageFile.url(), index: i };
     }
     return imageURLs;
   }
@@ -101,23 +164,52 @@ Template.edit.helpers({
 
 Template.edit.events({
   "change #studiesDropDown": function () {
-    Session.set("currentStudy", $("#studiesDropDown option:selected").val());
+    setCurrentStudy($("#studiesDropDown option:selected").val());
   },
   "click #editStudy": function () {
     // make fields editable
-    setFieldsReadOnly(false);
+    setFieldsDisabled(false);
     displayCancelSaveButton();
   },
   "click #cancelStudy": function () {
     // reset variables manually, not reactive here as currentStudy unchanged
     resetAllFields();
-    setFieldsReadOnly(true);
+    setFieldsDisabled(true);
     displayEditButton();
   },
   "click #saveStudy": function () {
     if (saveAllFields()) {
-      setFieldsReadOnly(true);
+      setFieldsDisabled(true);
       displayEditButton();
     }
+  },
+  "change #allTagsDropDown": function () {
+    if ($("#allTagsDropDown option:selected").val() === "Custom...") {
+      $("#addStudyTagField").css("display", "inline");
+    } else {
+      $("#addStudyTagField").css("display", "none");
+    }
+  },
+  "click #addStudyTagButton": function () {
+    var studyTag = $("#allTagsDropDown option:selected").val();
+    if (studyTag === "Custom...") {
+      studyTag = $("#addStudyTagField").val();
+    }
+    if (studyTag && (jQuery.inArray(studyTag, editStudyTags) < 0)) {
+      editStudyTags.push(studyTag);
+      Session.set("updateReactive", "added " + studyTag);
+    }
+  },
+   "click .studyTagButton": function () {
+     // get value of clicked tag
+     var studyTag = event.target.value;
+     editStudyTags.splice(jQuery.inArray(studyTag, editStudyTags), 1);
+     Session.set("updateReactive", "removed " + studyTag);
+  },
+  "click .swapImage": function () {
+    // convert value (index) to integer
+    var index = event.target.value * 1;
+    Session.set("updateReactive", swapImageWithNextImage(index));
+    console.log(index);
   }
 });
